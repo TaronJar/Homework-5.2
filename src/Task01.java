dd22d31efcb3a05461c52b4463e38e936e2e4fcc
taron <taron64arm@gmail.com>
2023-11-28 18:09:40 +0400

    add commit Task01.java

---
+++ src/Task01.java
@@ -0,0 +1,20 @@
+public class Task01 {
+    public static void main(String[] args) {
+        System.out.println("Задание: 1");
+        int[] fibonacciArray = new int[15];
+
+        // Заполняем массив числами Фибоначчи
+        fibonacciArray[0] = 0;
+        fibonacciArray[1] = 1;
+        for (int i = 2; i < 15; i++) {
+            fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+        }
+
+        // Выводим только четные элементы массива на консоль
+        for (int num : fibonacciArray) {
+            if (num % 2 == 0) {
+                System.out.print(num + "\t");
+            }
+        }
+    }
+}
